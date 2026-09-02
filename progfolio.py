57193b625664830f3141899494dc67e7c179898b
JangTV <ronjay.1204@gmail.com>
2026-09-02 16:44:57 +0800

    Fix font color for primary button to enhance visibility

--- progfolio.py
+++ progfolio.py
@@ -148,7 +148,7 @@ st.markdown("""
         border: none !important;
         transition: 0.2s ease !important;
         font-weight: 600 !important;
-        font-color: #f8fafc !important;
+        font-color: #hhhhhh !important;
     }
     .stButton>button[kind="primary"]:hover, [data-testid="stDownloadButton"] button:hover {
         background-color: #0284c7 !important;
